3e9a352a3c3dd7553cc7e9a5cc482d2a412d53a0
menachemkorf <menachemkorf@gmail.com>
2016-05-11 10:01:55 +0000

    Update input validations

--- lesson_2/mortgage_calculator.rb
+++ lesson_2/mortgage_calculator.rb
@@ -1,3 +1,7 @@
+def valid_number?(string)
+  string.to_f.to_s == string || string == "0"
+end
+
 def format_no_decimal(string)
   if string.length == 1
     ("0.0" + string).to_f
@@ -17,7 +21,9 @@ def format_with_decimal(string)
 end
 
 def formatted_apr(string)
-  if !string.include?('.')
+  if !valid_number?(string)
+    nil
+  elsif !string.include?('.')
     format_no_decimal(string)
   elsif string.include?('.')
     format_with_decimal(string)
@@ -28,11 +34,9 @@ puts "How much is the total loan?"
 loan_amount = ''
 loop do
   loan_amount = gets.to_i
-  if loan_amount > 0
-    break
-  else
-    puts "Please enter a valid number (more then 0)"
-  end
+  break unless loan_amount <= 0
+
+  puts "Please enter a valid number (more then 0)"
 end
 
 puts "Whats the Annual Percentage Rate (APR)? type 5 for 5%, 10.2 for 10.2%"
@@ -40,17 +44,22 @@ apr = ''
 loop do
   apr = gets.chomp
   apr = formatted_apr(apr)
-  if !apr.nil?
-    break
-  else
-    puts "Please enter a number between 0 and 100"
-  end
+
+  break unless apr.nil?
+
+  puts "Please enter a number between 0 and 100"
 end
 
 puts "How many years is the loan?"
-duration_years = gets.to_i
+duration_years = ''
+loop do
+  duration_years = gets.to_i
+  break unless duration_years <= 0
+
+  puts "Please enter a valid number (more then 0)"
+end
+
 interest_per_year = loan_amount * apr
-# interest_per_mounth = interest_per_year / 12
 total_interest = interest_per_year * duration_years
 total_due = loan_amount + total_interest
 total_per_year = total_due / duration_years
