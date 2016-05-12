10df083293b4ad2544dd74700a1d360596066ebc
menachemkorf <menachemkorf@gmail.com>
2016-05-12 10:14:16 +0000

    fix calculation

--- lesson_2/mortgage_calculator.rb
+++ lesson_2/mortgage_calculator.rb
@@ -14,32 +14,8 @@ def valid_number?(input)
   input.to_f.to_s == input || input.to_i.to_s == input
 end
 
-def format_no_decimal(input)
-  if input.length == 1
-    ("0.0" + input).to_f
-  elsif input.length == 2
-    ("0." + input).to_f
-  end
-end
-
-def format_with_decimal(input)
-  if input.index('.') == 1
-    input.slice!('.')
-    ("0.0" + input).to_f
-  elsif input.index('.') == 2
-    input.slice!('.')
-    ("0." + input).to_f
-  end
-end
-
-def formatted_apr(input)
-  if !valid_number?(input)
-    nil
-  elsif !input.include?('.')
-    format_no_decimal(input)
-  elsif input.include?('.')
-    format_with_decimal(input)
-  end
+def valid_apr?(input)
+  valid_number?(input) && input.to_i >= 0 && input.to_i < 100
 end
 
 puts "Welcome to the Mortgage Calculator!"
@@ -48,7 +24,7 @@ loop do
   prompt "How much is the total loan?"
   loan_amount = ''
   loop do
-    loan_amount = gets.to_i
+    loan_amount = gets.to_f
     break unless loan_amount <= 0
 
     error "Invalid number"
@@ -57,20 +33,22 @@ loop do
 
   prompt "Whats the Annual Percentage Rate (APR)? type 5 for 5%, 10.2 for 10.2%"
   apr = ''
+
   loop do
     apr = gets.chomp
-    apr = formatted_apr(apr)
-
-    break unless apr.nil?
-
-    error "Invalid number"
-    prompt "Please enter a number between 0 and 100"
+    if valid_apr?(apr)
+      apr = apr.to_f / 100
+      break
+    else
+      error "Invalid number"
+      prompt "Please enter a number between 0 and 100"
+    end
   end
 
   prompt "How many years is the loan?"
   duration_years = ''
   loop do
-    duration_years = gets.to_i
+    duration_years = gets.to_f
     break unless duration_years <= 0
 
     error "Invalid number"
@@ -79,15 +57,13 @@ loop do
 
   puts "Calculating..."
 
-  duration_months   = duration_years * 12
-  interest_per_year = loan_amount * apr
-  total_interest    = interest_per_year * duration_years
-  total_due         = loan_amount + total_interest
-  total_per_year    = total_due / duration_years
-  total_per_month   = total_per_year / 12
+  duration_months = duration_years * 12
+
+  r = apr / 12
+  monthly_payment = (r + r / ((1 + r)**duration_months - 1)) * loan_amount
 
   result "Loan duration: #{duration_months} months"
-  result "Monthly payments: $#{total_per_month}"
+  result "Monthly payments: $#{monthly_payment}"
 
   prompt "Another calculation? (y/n)"
   answer = ''
