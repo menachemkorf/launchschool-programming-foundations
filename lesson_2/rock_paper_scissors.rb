5dd4505096bb2e59d4b5755aa6de9ce982b9534d
menachemkorf <menachemkorf@gmail.com>
2016-05-14 13:40:18 +0000

    put winning points in constant

--- lesson_2/rock_paper_scissors.rb
+++ lesson_2/rock_paper_scissors.rb
@@ -1,7 +1,7 @@
 VALID_CHOICES = { 'r' => 'rock',
                   'p' => 'paper',
                   's' => 'scissors' }
-
+POINTS_TO_WIN = 5
 def prompt(msg)
   puts "#{msg} >>"
 end
@@ -45,25 +45,25 @@ loop do
       end
     end
 
-    computer_coice = VALID_CHOICES.values.sample
+    computer_choice = VALID_CHOICES.values.sample
 
-    display_result("You chose '#{player_choice}', computer chose '#{computer_coice}'.")
+    display_result("You chose '#{player_choice}', computer chose '#{computer_choice}'.")
 
-    if won?(player_choice, computer_coice)
+    if won?(player_choice, computer_choice)
       points[:player] += 1
-      display_result "You win"
-    elsif won?(computer_coice, player_choice)
+      display_result "You win!"
+    elsif won?(computer_choice, player_choice)
       points[:computer] += 1
-      display_result "Computer wins"
+      display_result "Computer wins!"
     else
       display_result "It's a tie!"
     end
-    display_result("You have #{points[:player]} points, computer has #{points[:computer]} points")
+    display_result("You have #{points[:player]} points, computer has #{points[:computer]} points.")
 
-    if points[:player] == 5
+    if points[:player] == POINTS_TO_WIN
       puts "You won the game!"
       break
-    elsif points[:computer] == 5
+    elsif points[:computer] == POINTS_TO_WIN
       puts "Game over!"
       break
     end
