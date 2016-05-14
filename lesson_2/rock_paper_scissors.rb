4d0af1e1d73f5f3ce6d07542e5aa77b179760dd0
menachemkorf <menachemkorf@gmail.com>
2016-05-14 12:42:51 +0000

    clean up code

--- lesson_2/rock_paper_scissors.rb
+++ lesson_2/rock_paper_scissors.rb
@@ -1,6 +1,6 @@
-VALID_CHICES_H = {  'r' => 'rock',
-                    'p' => 'paper',
-                    's' => 'scissors' }
+VALID_CHOICES = { 'r' => 'rock',
+                  'p' => 'paper',
+                  's' => 'scissors' }
 def prompt(msg)
   puts "#{msg} >>"
 end
@@ -27,31 +27,47 @@ Choose one:
 MSG
 
 loop do
-  player_choice = ''
+  points = { player: 0, computer: 0 }
+
   loop do
-    prompt choose_prompt
-    player_choice = gets.chomp.downcase
-    if VALID_CHICES_H.flatten.include?(player_choice)
-      VALID_CHICES_H.each do |k, v|
-        player_choice = v if player_choice == k
+    player_choice = ''
+    loop do
+      prompt choose_prompt
+      player_choice = gets.chomp.downcase
+      if VALID_CHOICES.flatten.include?(player_choice)
+        VALID_CHOICES.each do |k, v|
+          player_choice = v if player_choice == k
+        end
+        break
+      else
+        display_error "Invalid choice"
       end
-      break
-    else
-      display_error "Invalid choice"
     end
-  end
 
-  computer_coice = VALID_CHICES_H.values.sample
+    computer_coice = VALID_CHOICES.values.sample
 
-  display_result("You chose #{player_choice}")
-  display_result("Computer chose #{computer_coice}")
+    display_result("You chose #{player_choice}")
+    display_result("Computer chose #{computer_coice}")
 
-  if won?(player_choice, computer_coice)
-    display_result "You won!"
-  elsif won?(computer_coice, player_choice)
-    display_result "Computer won!"
-  else
-    display_result "It's a tie!"
+    if won?(player_choice, computer_coice)
+      points[:player] += 1
+      display_result "You win"
+    elsif won?(computer_coice, player_choice)
+      points[:computer] += 1
+      display_result "Computer wins"
+    else
+      display_result "It's a tie!"
+    end
+    display_result("You have #{points[:player]} points")
+    display_result("Computer has #{points[:computer]} points")
+
+    if points[:player] == 5
+      puts "You won the game!"
+      break
+    elsif points[:computer] == 5
+      puts "Game over!"
+      break
+    end
   end
 
   prompt "Do you want to play again? (y/n)"
