aefa3b375790008537b05e707834b5d5486e1821
menachemkorf <menachemkorf@gmail.com>
2016-05-15 03:35:06 +0000

    refactor prompt methods

--- lesson_2/rock_paper_scissors.rb
+++ lesson_2/rock_paper_scissors.rb
@@ -11,16 +11,16 @@ WINNING_COMBOS = {  'rock'      => %w(scissors lizard),
                     'spock'     => %w(rock scissors) }
 POINTS_TO_WIN = 5
 
-def prompt(msg)
-  puts "#{msg} >>"
-end
-
-def display_result(msg)
-  puts "=> #{msg}"
-end
-
-def display_error(msg)
-  puts "! #{msg}"
+def prompt(msg, format = '')
+  if format == :prompt
+    puts "#{msg} >>"
+  elsif format == :result
+    puts "=> #{msg}"
+  elsif format == :error
+    puts "! #{msg}"
+  else
+    puts msg
+  end
 end
 
 def won?(player1, player2)
@@ -33,10 +33,11 @@ loop do
   loop do
     player_choice = ''
     loop do
-      puts "Choose one:"
+      prompt "Choose one:"
       VALID_CHOICES.each do |k, v|
-        puts "#{k}. #{v}"
+        prompt "#{k}. #{v}"
       end
+      prompt("", :prompt)
       player_choice = gets.chomp.downcase
       if VALID_CHOICES.flatten.include?(player_choice)
         VALID_CHOICES.each do |k, v|
@@ -44,45 +45,45 @@ loop do
         end
         break
       else
-        display_error "Invalid choice"
+        prompt("Invalid choice", :error)
       end
     end
 
     computer_choice = VALID_CHOICES.values.sample
 
-    display_result("You chose '#{player_choice}', computer chose '#{computer_choice}'.")
+    prompt("You chose '#{player_choice}', computer chose '#{computer_choice}'.", :result)
 
     if won?(player_choice, computer_choice)
       points[:player] += 1
-      display_result "You win!"
+      prompt("You win!", :result)
     elsif won?(computer_choice, player_choice)
       points[:computer] += 1
-      display_result "Computer wins!"
+      prompt("Computer wins!", :result)
     else
-      display_result "It's a tie!"
+      prompt("It's a tie!", :result)
     end
-    display_result("You have #{points[:player]} points, computer has #{points[:computer]} points.")
+    prompt("You have #{points[:player]} points, computer has #{points[:computer]} points.", :result)
 
     if points[:player] == POINTS_TO_WIN
-      puts "You won the game!"
+      prompt "You won the game!"
       break
     elsif points[:computer] == POINTS_TO_WIN
-      puts "Game over!"
+      prompt "Game over!"
       break
     end
   end
 
-  prompt "Do you want to play again? (y/n)"
+  prompt("Do you want to play again? (y/n)", :prompt)
   answer = ''
   loop do
     answer = gets.chomp.downcase
     if answer == 'y' || answer == 'n'
       break
     else
-      display_error "Invalid option."
-      prompt "Please enter a valid option (y/n)"
+      prompt("Invalid option.", :error)
+      prompt("Please enter a valid option (y/n)", :prompt)
     end
   end
   break if answer == 'n'
 end
-puts "Thank you for playing. Good bye!"
+prompt("Thank you for playing. Good bye!")
